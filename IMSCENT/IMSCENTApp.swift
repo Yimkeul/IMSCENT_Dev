da5110d83b9a811cb223f2b465094aa05e2276b5
Yimkeul <leesjun29@gmail.com>
2023-09-16 16:22:54 +0900

    upload

---
+++ IMSCENT/IMSCENTApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMSCENTApp.swift
+//  IMSCENT
+//
+//  Created by yimkeul on 2023/09/16.
+//
+
+import SwiftUI
+
+@main
+struct IMSCENTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
